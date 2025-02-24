d9497ffdea2823f77aa1e6804a98146700f67b0b
hngocs <ngoclnah22411c@st.uel.edu.vn>
2025-02-24 18:07:25 +0700

    N- header/footer-create

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomepageComponent } from './pages/websites/homepage/homepage.component';
+import { IndexComponent } from './pages/partials/index/index.component';
+const routes: Routes = [
+    {path:'', component:IndexComponent,
+      children: [
+        {path:'', component:HomepageComponent}
+      ] 
+    },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
