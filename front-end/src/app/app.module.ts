9d31496c97802d7a099edc56524a0961274b773b
hngocs <ngoclnah22411c@st.uel.edu.vn>
2025-02-24 18:07:25 +0700

    N- header/footer-create

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/partials/header/header.component';
+import { FooterComponent } from './pages/partials/footer/footer.component';
+import { HomepageComponent } from './pages/websites/homepage/homepage.component';
+import { IndexComponent } from './pages/partials/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomepageComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
